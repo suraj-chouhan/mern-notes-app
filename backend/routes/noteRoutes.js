ce582310a867dcdf64187f2fdad1ddebea4b566f
surajchouhan1845 <gm.surajchouhan1845@gmail.com>
2026-06-01 21:03:04 +0530

    Fixed controller path case issue

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getNotes, addNote, deleteNote, updateNote } = require("../controllers/notecontroller");
+const { getNotes, addNote, deleteNote, updateNote } = require("../controllers/noteController");
 
 router.get("/notes" , getNotes);
 
