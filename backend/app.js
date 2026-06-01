568773921c1001ef3c50dfa9a62803f037639970
surajchouhan1845 <gm.surajchouhan1845@gmail.com>
2026-06-01 20:57:48 +0530

    Fix render deployment

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -11,6 +12,8 @@ const noteRoutes = require("./routes/noteRoutes");
 
 app.use("/api",noteRoutes);
 
-app.listen(3000,()=>{
-	console.log("Server running on 3000 port");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server running on ${PORT}`);
 });
--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/note-app").then(()=> console.log("db connected")).catch((err)=>console.log(err));
-
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
 
